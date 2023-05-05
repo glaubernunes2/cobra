3c4f58d34cb8575d3589a360237344e781cbf566
glaubernunes2 <119985120+glaubernunes2@users.noreply.github.com>
2023-05-05 15:53:12 -0300

    tradução

--- index.js
+++ index.js
@@ -1,102 +1,102 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelectorAll(".controls i");
+const controles = document.querySelectorAll(".controles i");
 
 let gameOver = false;
-let foodX, foodY;
-let snakeX = 5, snakeY = 5;
-let velocityX = 0, velocityY = 0;
-let snakeBody = [];
+let comidaX, foodY;
+let cobraX = 5, cobraY = 5;
+let velocidadeX = 0, velocidadeY = 0;
+let corpoCobra = [];
 let setIntervalId;
 let score = 0;
 
-// Get high score from local storage
+// Obtenha pontuação alta do armazenamento local
 
 let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
 
-// Pass a random between 1 and 30 as food position
+// Passe um aleatório entre 1 e 30 como posição de alimento
 
-const updateFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) + 1;
+const posicaoComida = () => {
+    comidaX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
-const handleGameOver = () => {
+const mensagemGameOver = () => {
     clearInterval(setIntervalId);
-    alert("Game Over! Press OK to replay...");
+    alert("Game Over! Preciona OK para continuar...");
     location.reload();
 }
 
-// Change velocity value based on key press
+// Alterar o valor da velocidade com base no pressionamento da tecla
 
-const changeDirection = e => {
-    if (e.key === "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    } else if (e.key === "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
-    } else if (e.key === "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
-    } else if (e.key === "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
+const mudacaDirecao = e => {
+    if (e.key === "ArrowUp" && velocidadeY != 1) {
+        velocidadeX = 0;
+        velocidadeY = -1;
+    } else if (e.key === "ArrowDown" && velocidadeY != -1) {
+        velocidadeX = 0;
+        velocidadeY = 1;
+    } else if (e.key === "ArrowLeft" && velocidadeX != 1) {
+        velocidadeX = -1;
+        velocidadeY = 0;
+    } else if (e.key === "ArrowRight" && velocidadeX != -1) {
+        velocidadeX = 1;
+        velocidadeY = 0;
     }
 }
 
-// Change Direction on each key click
+// Alterar direção em cada clique de tecla
 
-controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+controles.forEach(button => button.addEventListener("click", () => mudacaDirecao({ key: button.dataset.key })));
 
-const initGame = () => {
-    if (gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+const inicioGame = () => {
+    if (gameOver) return mensagemGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${comidaX}"></div>`;
 
-    // When snake eat food
-    if (snakeX === foodX && snakeY === foodY) {
-        updateFoodPosition();
-        snakeBody.push([foodY, foodX]); //Add food to snake body array
+    // Quando cobra come comida
+    if (cobraX === comidaX && cobraY === foodY) {
+        posicaoComida();
+        corpoCobra.push([foodY, comidaX]); //Adicionar comida à matriz do corpo da cobra
         score++;
-        highScore = score >= highScore ? score : highScore; // if score > high score => high score = score
+        highScore = score >= highScore ? score : highScore; // se score > high score => high score = score
 
         localStorage.setItem("high-score", highScore);
         scoreElement.innerText = `Score: ${score}`;
         highScoreElement.innerText = `High Score: ${highScore}`;
     }
 
-    // Update Snake Head
-    snakeX += velocityX;
-    snakeY += velocityY;
+    // Atualizar Snake Head
+    cobraX += velocidadeX;
+    cobraY += velocidadeY;
 
-    // Shifthing forward values of elements in snake body by one
+    // Valores de avanço de elementos no corpo da cobra por um
 
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
+    for (let i = corpoCobra.length - 1; i > 0; i--) {
+        corpoCobra[i] = corpoCobra[i - 1];
     }
 
-    snakeBody[0] = [snakeX, snakeY];
+    corpoCobra[0] = [cobraX, cobraY];
 
-    // Check snake body is out of wall or no
+    // Verifique se o corpo da cobra está fora da parede ou não
 
-    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    if (cobraX <= 0 || cobraX > 30 || cobraY <= 0 || cobraY > 30) {
         return gameOver = true;
     }
 
-    // Add div for each part of snake body
+    // Adicione div para cada parte do corpo da cobra
 
-    for (let i = 0; i < snakeBody.length; i++) {
-        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-        // Check snake head hit body or no
-        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+    for (let i = 0; i < corpoCobra.length; i++) {
+        html += `<div class="head" style="grid-area: ${corpoCobra[i][1]} / ${corpoCobra[i][0]}"></div>`;
+        // Verifique se a cabeça da cobra atingiu o corpo ou não
+        if (i !== 0 && corpoCobra[0][1] === corpoCobra[i][1] && corpoCobra[0][0] === corpoCobra[i][0]) {
             gameOver = true;
         }
     }
     playBoard.innerHTML = html;
 }
 
-updateFoodPosition();
-setIntervalId = setInterval(initGame, 100);
-document.addEventListener("keyup", changeDirection);
+posicaoComida();
+setIntervalId = setInterval(inicioGame, 100);
+document.addEventListener("keyup", mudacaDirecao);
